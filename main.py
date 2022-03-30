595c61657e4c6446d6755603a3e43c232d484ede
Sachin Lendis <you@example.com>
2022-03-30 11:36:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+header = st.container()
+dataset = st.container()
+#slider = st.sidebar()
+
+with header:
+    st.title('Welcome to the PSV⚾ H2 Stats📊 Web App🤖')
+    st.caption('Web App to display current PSV H2 player and team stats')
